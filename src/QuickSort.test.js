732cbd652df666ae59a0e36233bd4517a4b58a39
Zach Boone <zach@predictspring.com>
2019-06-08 22:07:32 -0700

    add unit tests

--- src/QuickSort.test.js
+++ src/QuickSort.test.js
@@ -1,4 +1,5 @@
 import QuickSort from './QuickSort';
+import { swap } from './QuickSort';
 
 const quickSort = new QuickSort();
 
@@ -11,5 +12,18 @@ describe('QuickSort.getStates', () => {
   it('the ending state should contain the sorted array', () => {
     const endIndex = states.length - 1;
     expect(states[endIndex].arr).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9]);
+  });
+});
+
+describe('swap', () => {
+  it('swaps two adjacent elements in an array', () => {
+    const arr = [2, 1];
+    swap(arr, 0, 1);
+    expect(arr).toEqual([1, 2]);
+  });
+  it('swaps any two elements in an array', () => {
+    const arr = [2, 1, 4, 9, 8];
+    swap(arr, 1, 3);
+    expect(arr).toEqual([2, 9, 4, 1, 8]);
   });
 });
