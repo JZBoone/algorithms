5eb91ee37c1e34853a6f2505ad448eb1a6fcea80
Zach Boone <zach@predictspring.com>
2019-06-08 21:56:32 -0700

    update unit test

--- src/QuickSort.test.js
+++ src/QuickSort.test.js
@@ -1,7 +1,15 @@
 import QuickSort from './QuickSort';
 
-it('should sort an array of integers', () => {
+const quickSort = new QuickSort();
+
+describe('QuickSort.getStates', () => {
   const unsorted = [5, 3, 9, 7, 1, 4, 8, 6, 2];
-  const result = QuickSort.sort(unsorted, 0, 8);
-  expect(result).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9]);
+  const states = quickSort.getStates(unsorted);
+  it('the starting state should contain the unsorted array', () => {
+    expect(states[0].arr).toEqual([5, 3, 9, 7, 1, 4, 8, 6, 2]);
+  });
+  it('the ending state should contain the sorted array', () => {
+    const endIndex = states.length - 1;
+    expect(states[endIndex].arr).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9]);
+  });
 });
